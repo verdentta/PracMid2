e33bbf8ae89a8a5da8972908c49e73326e36ddfa
Romeo Paul <62362806+verdentta@users.noreply.github.com>
2023-03-11 04:28:35 -0500

    Update test_sample.py

--- test_sample.py
+++ test_sample.py
@@ -3,7 +3,7 @@ Test helper functions
 """
 
 import unittest
-from sample import increment_by_two
+from sample import increment_by_two, increment_by_three
 
 class TestSampleMethods(unittest.TestCase):
     """
