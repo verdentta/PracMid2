f037c409693a27a272b46b03611af1f962d71fa9
Romeo Paul <62362806+verdentta@users.noreply.github.com>
2023-03-11 03:33:17 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,1 +1,12 @@
+"""
+Helper functions live here
+"""
 
+
+def increment_by_two(var_to_increment):
+    """
+    This function increments input by two
+    :param var_to_increment: param to increment
+    :return: incremented variable
+    """
+    return var_to_increment + 2
